e5db5d8ee38d37ef97dde8a4bfc2df9277e15883
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-10-28 05:26:39 -0700

    Initial SabraRonins.java

---
+++ SabraRonins.java
@@ -0,0 +1,62 @@
+package cis;
+import robocode.*;
+//import java.awt.Color;
+
+/**
+ * Sabra Bot
+ */
+public class SabraRonins extends AlphaBot
+{
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			turnRight(5000);
+			
+			ahead(10000);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(3);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+	
+	public void onHitRobot(HitRobotEvent event) {
+       if (event.getBearing() > -90 && event.getBearing() <= 90) {
+           back(100);
+       } else {
+           ahead(100);
+       }
+	   
+		if (event.isMyFault()) {
+			turnRight(10);
+		}
+   }
+
+}
